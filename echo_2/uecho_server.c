79646b7487f2b96b652f433873620f55051e3099
Minseo Park <80569061+M1nseoPark@users.noreply.github.com>
2023-01-21 21:34:58 +0900

    Update uecho_server.c

--- echo_2/uecho_server.c
+++ echo_2/uecho_server.c
@@ -21,7 +21,7 @@ int main(int argc, char* argv[]) {
         exit(1);
     }
 
-    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);
+    serv_sock = socket(PF_INET, SOCK_DGRAM, 0);   // UDP 소켓 생성 
     if (serv_sock == -1)
         error_handling("UDP socket creation error");
 
@@ -30,13 +30,13 @@ int main(int argc, char* argv[]) {
     serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_adr.sin_port = htons(atoi(argv[1]));
 
-    if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1 )
+    if (bind(serv_sock, (struct sockaddr*)&serv_adr, sizeof(serv_adr)) == -1)
         error_handling("bind() error");
 
-    while(1) {
+    while(1) {   
         clnt_adr_sz = sizeof(clnt_adr);
-        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);
-        sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);
+        str_len = recvfrom(serv_sock, message, BUF_SIZE, 0, (struct sockaddr*)&clnt_adr, &clnt_adr_sz);   // bind 함수에서 할당된 주소로 전달되는 모든 데이터를 수신함 
+        sendto(serv_sock, message, str_len, 0, (struct sockaddr*)&clnt_adr, clnt_adr_sz);   
     }
     
     close(serv_sock);
